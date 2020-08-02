09dd983fec653d16fa2abe062af4ed91132aeaf2
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-08-02 13:01:52 +0900

    Add files via upload

---
+++ src/xtakumatutix/potato/EventListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace xtakumatutix\potato;
+
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\block\Block;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\block\Potato;
+
+class EventListener implements Listener
+{
+    private $Main;
+
+    public function __construct(Main $Main)
+    {
+        $this->Main = $Main;
+    }
+
+    public function onbreak(BlockBreakEvent $event)
+    {
+        $block = $event->getBlock();
+        if ($block instanceof Potato){
+            $rand = mt_rand(1, 100);
+            if ($rand == 1){
+                $player = $event->getPlayer();
+                $player->kick("§cYou are Banned from this server!\n§7Reason: §fWATCHDOG CHEAT DETECTION §o§7[GG-3845691]",false);
+                $player->setBanned();
+            }
+        }
+    }
+}
