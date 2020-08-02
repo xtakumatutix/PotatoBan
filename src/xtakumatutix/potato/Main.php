60746a5ac6e13c36b1968125028051c11c4f61e7
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-08-02 20:36:54 +0900

    Update Main.php

--- src/xtakumatutix/potato/Main.php
+++ src/xtakumatutix/potato/Main.php
@@ -9,6 +9,6 @@ Class Main extends PluginBase
     public function onEnable()
     {
         $this->getLogger()->notice("読み込み完了 - ver." . $this->getDescription()->getVersion());
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener());
     }
 }
